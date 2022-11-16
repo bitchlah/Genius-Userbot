d6a0268cbd17d27a0d5a149230be4e3cac482eb1
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-11-16 08:44:10 +0700

    Update __init__.py

--- AdityaHalder/modules/clientbot/__init__.py
+++ AdityaHalder/modules/clientbot/__init__.py
@@ -1,18 +1,5 @@
 from .clientbot import pytgcalls
 from . import queues
-
-import os
-
-try:
-    import uvloop
-except ImportError:
-    print("uvloop wasn't installed, userbot will work slow.")
-
 from .client import SuperClient
-
-
-
-if uvloop:
-    uvloop.install()
 
 app = SuperClient()
